554af714d14b6b78da6ae214e465f9d9980090be
Ripazha1333 <nhada1333@gmail.com>
2023-11-27 11:17:09 -0600

    agregada la clase criptograma

--- criptograma.hpp
+++ criptograma.hpp
@@ -1,1 +1,20 @@
 #pragma once
+
+#include <String>
+
+class Criptograma {
+
+    String mensaje;
+
+
+    public:
+
+    Criptograma(            );
+    Criptograma( String msg );
+
+    void cesar(    );
+    void vigenere( );
+
+    void set_message( String msg );
+    String get_message(          );
+}
